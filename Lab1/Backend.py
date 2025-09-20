26421fac643663f40d653c07ca77501ba8feb796
BarreIMan <trannguyenkhaicnk@gmail.com>
2025-09-20 20:20:11 +0700

    + Tạo ra file Backend để chứa FakeAI + FakeAI chừ có khả năng trả lời người dùng. + Thay đổi file GUI.txt thành GUI.py... ??? + Thay đổi dòng logic trong phần tả lời cuối của AI sau khi chọn màu, chừ thay vì (chọn màu -> "AI" gửi tin nhắn thử lại) thì là (chọn màu -> "AI" gửi trả lời dựa trên lựa chọn người dùng -> "AI" gửi tin nhắn thử lại) + Một số thay đổi nhỏ khác.

---
+++ Lab1/Backend.py
@@ -0,0 +1,52 @@
+from enum import Enum
+
+# Cứ hiểu Enum là một dạng list mà đều OOP đi.
+# Lý do vì sao tụi mình nên chọn xài Enum: Cho ngầu B).
+
+class GenderComments(Enum):
+    Male= "Male"
+    Female= "Female"
+
+class OccasionComments(Enum):
+    Date= "Date"
+    Wedding= "Wedding"
+    FamilyMeeting= "FamilyMeeting"
+    Holiday= "Holiday"
+    School= "School"
+    Other= "Other"
+
+class ColorComments(Enum):
+    Black= "Black"
+    White= "White"
+    Blue= "Blue"
+    Red= "Red"
+    Green= "Green"
+    Yellow= "Yellow"
+
+def GetCommentsFromFakeAI(input: list[str]) -> str:
+    GENDER_INPUT = input[0]
+    OCCASION_INPUT = input[1]
+    COLOR_INPUT = input[2]
+
+    res = ""
+
+    # TODO: Ba cái loop cơ chế giống nhau thì nên viết một function.
+    def GetCommentForInput(input: str, comments: Enum):
+        for item in (comments):
+            input_matches: bool = (item.name == input)
+
+            if input_matches:
+                comment: str = item.value + " "
+                
+                return comment
+            
+    try:
+            res += GetCommentForInput(input= GENDER_INPUT, comments= GenderComments)
+            res += GetCommentForInput(input= OCCASION_INPUT, comments= OccasionComments)
+            res += GetCommentForInput(input= COLOR_INPUT, comments= ColorComments)  
+            
+    
+    except Exception as e:
+        print(e)
+    
+    return res
